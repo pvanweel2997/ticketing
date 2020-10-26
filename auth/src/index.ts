eeb90c68176e6702746c92cf1442e2133ed58188
Patrick Van Weelden <pvanweel@yahoo.com>
2020-10-26 15:40:09 -0500

    ran npm install express-async-errors to handle async errors.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -7,6 +8,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 import { errorHanlder } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 
 const app = express();
@@ -16,6 +18,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+})
+
 app.use(errorHanlder);
 
 app.listen(3000, () => {
