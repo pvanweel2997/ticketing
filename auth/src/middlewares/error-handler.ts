429c5c01c805570e8fc5ac16b29de5370ad7e44e
Patrick Van Weelden <pvanweel@yahoo.com>
2020-10-26 15:19:43 -0500

    More validation handling changes by using CustomError abstract class

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHanlder = (
   err: Error, 
@@ -8,13 +7,9 @@ export const errorHanlder = (
   res: Response, 
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    console.log('handling this RequestValidationError');
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors()});
   }
-
-  if (err instanceof DatabaseConnectionError) {
-    res.status(500).send({ errors: [{ message: err.reason}] })
-  }
-
+ 
   res.status(400).send({ errors: [{ message: 'Something went wrong'}] })
 };
