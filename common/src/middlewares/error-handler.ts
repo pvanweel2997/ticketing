36ae1aa3c7b682367bd7abea7bdb8de8063bae88
Patrick Van Weelden <pvanweel@yahoo.com>
2020-11-04 11:42:05 -0600

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -10,6 +10,6 @@ export const errorHanlder = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors()});
   }
- 
+  console.log(err);
   res.status(400).send({ errors: [{ message: 'Something went wrong'}] })
 };
