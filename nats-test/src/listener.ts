12224fcedae02d31481ce2050a3a8f687b962a20
Patrick Van Weelden <pvanweel@yahoo.com>
2020-11-09 15:32:30 -0600

    Change to strong type our events and subjects for our listeners and publishers.

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,30 @@
 import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
-const stan = nats.connect('ticketing','123', {
+
+
+const stan = nats.connect('ticketing',randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
 stan.on('connect',() => {
   console.log('listener connected to NATS');
-  
-  const subscription = stan.subscribe('ticket:created');
 
-  subscription.on('message',(msg) => {
-    console.log('message received');
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit;
   })
+  
+  new TicketCreatedListener(stan).listen();
+  
 });
+
+// check for signal interrupt or signal termination
+process.on('SIGINT',() =>   stan.close());
+process.on('SIGTERM',() =>   stan.close());
+
+
+
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedListener } from './events/ticket-created-listener';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher'; 
 
 console.clear();
 
@@ -6,16 +8,27 @@ const stan = nats.connect('ticketing','abc',{
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('publisher connected to nats');
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'conert',
+      price: 20
+    });
+  } catch(err) {
+    console.error(err);
+  }
 
-  stan.publish('ticket:created',data, () => {
-    console.log('event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20
+  // });
+
+  // stan.publish('ticket:created',data, () => {
+  //   console.log('event published');
+  // });
 
 });
